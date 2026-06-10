c1b78c40932e91e0bcca2cefd13bb122bb8b634c
Serena <93484786+serena351@users.noreply.github.com>
2026-06-10 11:04:36 +0100

    Generate fake product data
    Added fields to create fake products with various attributes.

--- tests/factories.py
+++ tests/factories.py
@@ -31,4 +31,28 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    name = FuzzyChoice(choices=[
+        "Hat",
+        "Trousers",
+        "Shirt",
+        "Apple",
+        "Orange",
+        "Pots",
+        "Towels",
+        "Mini",
+        "Ford",
+        "Hammer",
+        "Spanner"
+    ])
+    description = factory.Faker("text")
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(choices=[
+        Category.UNKNOWN,
+        Category.CLOTHES,
+        Category.FOOD,
+        Category.HOMEWARE,
+        Category.CARS,
+        Category.TOOLS
+    ])
+    
