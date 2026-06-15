72cebe3b08c15874d32f5c6b3113e9bd2755ae6b
Serena <93484786+serena351@users.noreply.github.com>
2026-06-15 10:36:13 +0100

    Update tests for product creation and deletion

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -130,19 +130,15 @@ class TestProductRoutes(TestCase):
         self.assertEqual(new_product["available"], test_product.available)
         self.assertEqual(new_product["category"], test_product.category.name)
 
-        #
-        # Uncomment this code once READ is implemented
-        #
-
-        # # Check that the location header was correct
-        # response = self.client.get(location)
-        # self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # new_product = response.get_json()
-        # self.assertEqual(new_product["name"], test_product.name)
-        # self.assertEqual(new_product["description"], test_product.description)
-        # self.assertEqual(Decimal(new_product["price"]), test_product.price)
-        # self.assertEqual(new_product["available"], test_product.available)
-        # self.assertEqual(new_product["category"], test_product.category.name)
+        # Check that the location header was correct
+        response = self.client.get(location)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        new_product = response.get_json()
+        self.assertEqual(new_product["name"], test_product.name)
+        self.assertEqual(new_product["description"], test_product.description)
+        self.assertEqual(Decimal(new_product["price"]), test_product.price)
+        self.assertEqual(new_product["available"], test_product.available)
+        self.assertEqual(new_product["category"], test_product.category.name)
 
     def test_create_product_with_no_name(self):
         """It should not Create a Product without a name"""
@@ -189,6 +185,19 @@ class TestProductRoutes(TestCase):
       self.assertEqual(response.status_code, status.HTTP_200_OK)
       updated_product = response.get_json()
       self.assertEqual(updated_product["description"], "unknown")
+
+    def test_delete_product(self):
+      """It should delete a product"""
+      products = self._create_product(5)
+      count = self.get_product_count()
+      test_product = products[0]
+      response = self.client.delete(f"{BASE_URL}/{test_product.id}")
+      self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+      self.assertEqual(len(response.data), 0)
+      response = self.client.get(f"{BASE_URL}/{test_product.id}")
+      self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+      newcount = self.get_product_count()
+      self.assertEqual(newcount, count - 1)
 
     ######################################################################
     # Utility functions
