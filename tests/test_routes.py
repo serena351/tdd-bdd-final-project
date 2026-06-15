a9ca9d933367370ddc86d6bf04fb0f152b664a38
Serena <93484786+serena351@users.noreply.github.com>
2026-06-15 09:28:53 +0100

    Add test_update_product

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -170,13 +170,25 @@ class TestProductRoutes(TestCase):
       self.assertEqual(response.status_code, status.HTTP_200_OK)
       data = response.get_json()
       self.assertEqual(data["name"], test_product.name)
+      
+    def test_get_product_not_found(self):
+      """It should return a 404 not found error"""
+      response = self.client.get(f"BASE_URL/0")
+      self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+      data = response.get_json()
+      self.assertIn("not found", data["message"])
 
-  def test_get_product_not_found(self):
-    """It should return a 404 not found error"""
-    response = self.client.get(f"BASE_URL/0")
-    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-    data = response.get_json()
-    self.assertIn("not found", data["message"])
+    def test_update_product(self):
+      """It should update an existing product"""
+      test_product = ProductFactory()
+      response = self.client.post(BASE_URL, json=test_product.serialize())
+      self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+      new_product = response.get_json()
+      new_product["description"] = "unknown" # update product
+      response = self.client.put(f"{BASE_URL}/{new_product['id']}", json=new_product)
+      self.assertEqual(response.status_code, status.HTTP_200_OK)
+      updated_product = response.get_json()
+      self.assertEqual(updated_product["description"], "unknown")
 
     ######################################################################
     # Utility functions
