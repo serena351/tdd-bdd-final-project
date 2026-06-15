2fcf340f5acbe651cc63aee9d2514e63f3566f76
Serena <93484786+serena351@users.noreply.github.com>
2026-06-15 08:30:58 +0100

    Implement GET endpoint
    Added endpoint to retrieve a product by its ID.

--- service/routes.py
+++ service/routes.py
@@ -105,10 +105,16 @@ def create_products():
 ######################################################################
 # R E A D   A   P R O D U C T
 ######################################################################
+@app.route("/products/<int:product_id>", methods=["GET"])
+def get_products(product_id):
+    """Retrieve a single product
+    This endpoint will return a product based on its id"""
+    app.logger.info("Request to retrieve a product with id [%s]", product_id)
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id {product_id} not found") # if product not found
+    return product.serialize(), status.HTTP_200_OK
 
-#
-# PLACE YOUR CODE HERE TO READ A PRODUCT
-#
 
 ######################################################################
 # U P D A T E   A   P R O D U C T
