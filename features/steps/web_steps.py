2b52ce97fe9bb395730461ae87c98f23cf34bcaf
Serena <93484786+serena351@users.noreply.github.com>
2026-06-22 14:03:46 +0100

    Implement 'success' message step

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -119,10 +119,20 @@ def step_impl(context, name):
     )    
     assert(found)
     
-#@then('I should not see "{name}" in the results')
-#def step_impl(context, name):
-#    context.driver.find_element_by_id('search_results')
-#    assert(found)
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element_by_id('search_results')
+    assert(name not in element.text) # assert name not found in results
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    ) 
+    assert(found)
 
 ##################################################################
 # This code works because of the following naming convention:
